f960525dc6dce4cf08014cd8f8bb4fb69cddeb14
jjthomps123 <jjthomps@sfu.ca>
2018-07-30 12:41:01 -0700

    Update correlateddata.R

--- day4/correlateddata.R
+++ day4/correlateddata.R
@@ -11,6 +11,8 @@ Sigma <- matrix(.35, nrow=2, ncol=2) + diag(2)*.65
 rawvars <- mvrnorm(n=33, mu=mu, Sigma=Sigma)
 
 #TODO: perform a correlation test with something like cor.test(x,y)$p.value
+Data=data.frame(rawvars)
+cor.test(Data$X1,Data$X2)$p.value
 
 #TODO: Create a function that runs this code 1 time
 
